e85ed82e7be349fbb88a1d435db757364fe72940
gyuminlee <gyumin.lee@bespinglobal.com>
2023-02-08 17:52:40 +0900

    Chore: 공통 환경 관련 설정 추가 및 수정
    h2 db -> mysql 문법 사용 설정
    JPA properties 적용을 위한 config 수정
    logging 관련 수정

--- src/main/java/com/easyoops/common/config/DataSourceConfig.java
+++ src/main/java/com/easyoops/common/config/DataSourceConfig.java
@@ -3,8 +3,11 @@ package com.easyoops.common.config;
 import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -13,7 +16,9 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.**.**.repository"})
@@ -41,19 +46,38 @@ public class DataSourceConfig {
 	//	}
 
 	@Primary
+	@Bean(name = "jpaProperties")
+	@ConfigurationProperties(prefix = "spring.jpa")
+	public JpaProperties jpaProperties() {
+		return new JpaProperties();
+	}
+
+	@Primary
 	@Bean( name = "entityManagerFactory" )
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("dataSource") DataSource dataSource,
+																	   @Qualifier("jpaProperties") JpaProperties jpaProperties) {
 		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-		entityManagerFactory.setDataSource(dataSource());
+		entityManagerFactory.setDataSource(dataSource);
+		entityManagerFactory.setPersistenceUnitName("default");
 		entityManagerFactory.setPackagesToScan("com.**.**.entity");
+		entityManagerFactory.setJpaPropertyMap(jpaProperties.getProperties());
 		entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 		return entityManagerFactory;
+//		return entityManagerFactoryBuilder
+//				.dataSource(dataSource)
+//				.properties(jpaProperties.getProperties())
+//				.packages("com.**.**.entity")
+//				.persistenceUnit("default")
+//				.build();
 	}
 
 	@Primary
 	@Bean(name = "transactionManager")
 	@DependsOn({"entityManagerFactory"})
-	PlatformTransactionManager transactionManager() {
-		return new JpaTransactionManager(Objects.requireNonNull(entityManagerFactory().getObject()));
+	PlatformTransactionManager transactionManager(@Qualifier("entityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+//		return new JpaTransactionManager(Objects.requireNonNull(entityManagerFactory().getObject()));
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(entityManagerFactory.getObject());
+//		jpaTransactionManager.setNestedTransactionAllowed(true);
+		return jpaTransactionManager;
 	}
 }
